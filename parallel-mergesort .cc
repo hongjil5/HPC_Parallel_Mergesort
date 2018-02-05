f4075c63961c3e577ab549a68af897276ab9f5d2
hongjil5 <hongjil5@uci.edu>
2018-02-04 16:38:39 -0800

    Update parallel-mergesort .cc

--- parallel-mergesort .cc
+++ parallel-mergesort .cc
@@ -1,9 +1,6 @@
 //============================================================================
-// Name        : test.cpp
-// Author      : Hongjian Lu
-// Version     :
-// Copyright   : start
-// Description : Hello World in C++, Ansi-style
+// Name        : parallel-mergesort.cc
+// Author      : Hongjian Lu, Ruihong Wang
 //============================================================================
 
 #include <iostream>
@@ -13,24 +10,22 @@
 #include <omp.h>
 #include "sort.hh"
 using namespace std;
-void MSort(keytype A[],keytype Temp[],int L,int RightEnd,bool srcToDst);
-//void Merge(keytype A[],int p1,int r1,int p2,int r2,keytype* B,int p3);
-int BinarySearch(int x,keytype A[],int p,int r)
+
+// Binary Search
+int BinarySearch(int x,keytype *A,int p,int r)
 {
     int low=p,mid,high;
     high=max(p,r+1);
-    while(low<high)
-    {
+    while(low<high){
         mid=(low+high)/2;
-        if(x<=A[mid])
-            high=mid;
-        else
-            low=mid+1;
+        if(x<=A[mid]) high=mid;
+        else low=mid+1;
     }
     return high;
 }
 
-void Merge(keytype A[],int p1,int r1,int p2,int r2,keytype* B,int p3)
+//Sequential Merge(Thread=1)
+void Merge(keytype *A,int p1,int r1,int p2,int r2,keytype* B,int p3)
 {
     int length = r1-p1+1+r2-p2+1;
     for(;p1<=r1&&p2<=r2;B[p3++]=(A[p1]<A[p2]?A[p1++]:A[p2++]));
@@ -38,146 +33,97 @@ void Merge(keytype A[],int p1,int r1,int p2,int r2,keytype* B,int p3)
     for(;p2<=r2;B[p3++]=A[p2++]);
 }
 
-void PMerge(keytype A[],int p1,int r1,int p2,int r2,keytype* B,int p3,int threads)
+//Parallel Merge(Thread>1)
+void PMerge(keytype* A,int p1,int r1,int p2,int r2,keytype* B,int p3,int threads)
 {
-if(threads>1){
-//cout<<threads<<'\n';
-    int q1,q2,q3;
-    int n1 = r1-p1+1;
-    int n2 = r2-p2+1;
-    if (n1<n2)
+    if(threads>1)
     {
-        swap(n1,n2);
-        swap(p1,p2);
-        swap(r1,r2);
-    }
-    if (n1==0)
-        return;
-    else
-    {
-        q1 = (p1+r1)/2;
-        q2 = BinarySearch(A[q1],A,p2,r2); //A is int or not
-        q3 = p3+(q1-p1)+(q2-p2);
-        B[q3]=A[q1];
-        #pragma omp parallel sections
+        int q1,q2,q3;
+        int n1 = r1-p1+1;
+        int n2 = r2-p2+1;
+        if (n1<n2){swap(n1,n2);swap(p1,p2);swap(r1,r2);}//Exchange to ensure n1>=n2
+        if (n1==0)
+            return;
+        else
         {
-          #pragma omp section
-{
- // #pragma omp task
-            PMerge(A,p1,q1-1,p2,q2-1,B,p3,threads/2);
-}
-	  #pragma omp section
-{
-            PMerge(A,q1+1,r1,q2,r2,B,q3+1,threads/2);
-}           
-// #pragma omp taskwait
+            q1 = (p1+r1)/2;
+            q2 = BinarySearch(A[q1],A,p2,r2); //A is int or not
+            q3 = p3+(q1-p1)+(q2-p2);
+            B[q3]=A[q1];
+            #pragma omp parallel sections
+            {
+                #pragma omp section
+                PMerge(A,p1,q1-1,p2,q2-1,B,p3,threads/2);
+                #pragma omp section
+                PMerge(A,q1+1,r1,q2,r2,B,q3+1,threads/2);
+            }
         }
     }
-}
-else 
-{
-    Merge( A,p1,r1,p2,r2,B,p3);
-} 
+    else Merge( A,p1,r1,p2,r2,B,p3);
 }
 
-void PMSort(keytype A[],keytype* B,int L,int RightEnd, int threads,bool srcToDst=true )
+//Sequential Mergesort(Thread=1)
+void MSort(keytype* A,keytype* B,int L,int RightEnd,bool srcToDst=true)
 {
-	//int length = RightEnd-L+1;
-	if ( L == RightEnd ) {    // termination/base case of sorting a single element
-			if ( srcToDst )  B[ L ] = A[ L ];    // copy the single element from src to dst
-			return;
-	}
-if(threads>1){
-//cout<<threads<<'\t';
+    if (L == RightEnd) {    // termination/base case of sorting a single element
+        if (srcToDst) B[L] = A[L];    // copy the single element from src to dst
+        return;
+    }
     int center;
     if(L<RightEnd)
     {
         center=(L+RightEnd)/2;
-        #pragma omp parallel sections
-        {
-           // #pragma omp task
-           #pragma omp section
-{
-            PMSort(A,B,L,center,threads/2,!srcToDst);
-}
-           #pragma omp section
-{
-            PMSort(A,B,center+1,RightEnd,threads/2,!srcToDst);
-           // #pragma omp taskwait
-}
-}
-        if ( srcToDst )PMerge(A,L,center,center+1,RightEnd,B,L,threads);
-        else PMerge(B,L,center,center+1,RightEnd,A,L,threads);
-    }
-}
-else{ MSort(A,B,L,RightEnd,srcToDst);}
-}
-
-void MSort(keytype A[],keytype* B,int L,int RightEnd,bool srcToDst=true)
-{
-    if ( L == RightEnd ) {    // termination/base case of sorting a single element
-        if ( srcToDst )  B[ L ] = A[ L ];    // copy the single element from src to dst
-        return;
-    }
-    int center;
-    if(L<RightEnd)
-    {   
-       center=(L+RightEnd)/2;
-       MSort(A,B,L,center,!srcToDst);
-       MSort(A,B,center+1,RightEnd,!srcToDst);
+        MSort(A,B,L,center,!srcToDst);
+        MSort(A,B,center+1,RightEnd,!srcToDst);
         if ( srcToDst )PMerge(A,L,center,center+1,RightEnd,B,L,1);
         else PMerge(B,L,center,center+1,RightEnd,A,L,1);
     }
-        
 }
 
-void Merge_sort(keytype A[],int N,int threads)
+//Parallel Mergesort(Thread>1)
+void PMSort(keytype* A,keytype* B,int L,int RightEnd, int threads,bool srcToDst=true )
+{
+	if (L == RightEnd) {    // termination/base case of sorting a single element
+        if (srcToDst)  B[L] = A[L];    // copy the single element from src to dst
+        return;
+	}
+    if(threads>1){
+        int center;
+        if(L<RightEnd){
+            center=(L+RightEnd)/2;
+            #pragma omp parallel sections
+            {
+               #pragma omp section
+               PMSort(A,B,L,center,threads/2,!srcToDst);
+               #pragma omp section
+               PMSort(A,B,center+1,RightEnd,threads/2,!srcToDst);
+            }
+            if (srcToDst)PMerge(A,L,center,center+1,RightEnd,B,L,threads);
+            else PMerge(B,L,center,center+1,RightEnd,A,L,threads);
+        }
+    }
+    else MSort(A,B,L,RightEnd,srcToDst);
+}
+
+//Mergesort
+void Merge_sort(keytype* A,int N,int threads)
 {
     keytype *Temp=(keytype *)malloc(N*sizeof(keytype));
     if(Temp)
     {                                                                   
        PMSort(A,Temp,0,N-1,threads);
-       for(int i=0;i<N;i++)
-                A[i]=Temp[i];
+       for(int i=0;i<N;i++) A[i]=Temp[i];
        free(Temp);
     }
-    else
-        printf("no space!\n");
+    else printf("no space!\n");
 }
 
 void parallelSort (int N, keytype* A)
 {
     //8 cores
-   omp_set_nested(1);
+    omp_set_nested(1);
     omp_set_num_threads(8);
     int threads =8;
-    
-Merge_sort(A,N,threads);
+    Merge_sort(A,N,threads);
 }
 
-
-/*
-void Merge(keytype A[],int p1,int r1,int p2,int r2,keytype* B,int p3)
-{   
-    int q1,q2,q3;
-    int n1 = r1-p1+1;
-    int n2 = r2-p2+1;
-    if (n1<n2)
-    {   
-        swap(n1,n2);
-        swap(p1,p2);
-        swap(r1,r2);
-    }
-    if (n1==0)
-        return;
-    else
-    {   
-        q1 = (p1+r1)/2;
-        q2 = BinarySearch(A[q1],A,p2,r2); //A is int or not
-        q3 = p3+(q1-p1)+(q2-p2);
-        B[q3]=A[q1];
-        
-        Merge(A,p1,q1-1,p2,q2-1,B,p3);
-        Merge(A,q1+1,r1,q2,r2,B,q3+1);
-    }
-}*/
